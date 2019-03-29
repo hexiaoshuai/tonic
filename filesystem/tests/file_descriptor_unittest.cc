e182f04d040e8a251f0715a1deac6b35bb9d53b0
Adam Barth <abarth@google.com>
2019-03-29 13:27:32 -0700

    Remove stray FXL dependency
    Change-Id: I347a539462cfbfd511e787baedfbfb6e122d9af4

--- filesystem/tests/file_descriptor_unittest.cc
+++ filesystem/tests/file_descriptor_unittest.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/fxl/portable_unistd.h"
-
 #include <fcntl.h>
 #include <sys/types.h>
 
