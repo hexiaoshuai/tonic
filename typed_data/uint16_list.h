bd27b4549199df72fcaeefd259ebc12a31c2e4ee
Matthew Dempsky <mdempsky@google.com>
2019-05-11 17:55:09 -0700

    Templatize the TypedData wrapper classes
    Change-Id: Icedb004338e390205fa6219ee18649ec3cc8febe

--- typed_data/uint16_list.h
+++ typed_data/uint16_list.h
@@ -5,58 +5,8 @@
 #ifndef LIB_TONIC_TYPED_DATA_UINT16_LIST_H_
 #define LIB_TONIC_TYPED_DATA_UINT16_LIST_H_
 
-#include "third_party/dart/runtime/include/dart_api.h"
-#include "tonic/converter/dart_converter.h"
+#warning uint16_list.h is deprecated; use typed_list.h instead.
 
-namespace tonic {
-
-// A simple wrapper around a Dart Uint16List. It uses Dart_TypedDataAcquireData
-// to obtain a raw pointer to the data, which is released when this object is
-// destroyed.
-//
-// This is designed to be used with DartConverter only.
-class Uint16List {
- public:
-  explicit Uint16List(Dart_Handle list);
-  Uint16List(Uint16List&& other);
-  Uint16List();
-  ~Uint16List();
-
-  uint16_t& at(intptr_t i) {
-    TONIC_CHECK(i < num_elements_);
-    return data_[i];
-  }
-  const uint16_t& at(intptr_t i) const {
-    TONIC_CHECK(i < num_elements_);
-    return data_[i];
-  }
-
-  uint16_t& operator[](intptr_t i) { return at(i); }
-  const uint16_t& operator[](intptr_t i) const { return at(i); }
-
-  const uint16_t* data() const { return data_; }
-  intptr_t num_elements() const { return num_elements_; }
-  Dart_Handle dart_handle() const { return dart_handle_; }
-
-  void Release();
-
- private:
-  uint16_t* data_;
-  intptr_t num_elements_;
-  Dart_Handle dart_handle_;
-
-  Uint16List(const Uint16List& other) = delete;
-};
-
-template <>
-struct DartConverter<Uint16List> {
-  static void SetReturnValue(Dart_NativeArguments args, Uint16List val);
-  static Uint16List FromArguments(Dart_NativeArguments args,
-                                 int index,
-                                 Dart_Handle& exception);
-  static Dart_Handle ToDart(const uint16_t* buffer, unsigned int length);
-};
-
-}  // namespace tonic
+#include "tonic/typed_data/typed_list.h"
 
 #endif  // LIB_TONIC_TYPED_DATA_UINT16_LIST_H_
--- typed_data/uint8_list.h
+++ typed_data/uint8_list.h
@@ -5,58 +5,8 @@
 #ifndef LIB_TONIC_TYPED_DATA_UINT8_LIST_H_
 #define LIB_TONIC_TYPED_DATA_UINT8_LIST_H_
 
-#include "third_party/dart/runtime/include/dart_api.h"
-#include "tonic/converter/dart_converter.h"
+#warning uint8_list.h is deprecated; use typed_list.h instead.
 
-namespace tonic {
-
-// A simple wrapper around a Dart Uint8List. It uses Dart_TypedDataAcquireData
-// to obtain a raw pointer to the data, which is released when this object is
-// destroyed.
-//
-// This is designed to be used with DartConverter only.
-class Uint8List {
- public:
-  explicit Uint8List(Dart_Handle list);
-  Uint8List(Uint8List&& other);
-  Uint8List();
-  ~Uint8List();
-
-  uint8_t& at(intptr_t i) {
-    TONIC_CHECK(i < num_elements_);
-    return data_[i];
-  }
-  const uint8_t& at(intptr_t i) const {
-    TONIC_CHECK(i < num_elements_);
-    return data_[i];
-  }
-
-  uint8_t& operator[](intptr_t i) { return at(i); }
-  const uint8_t& operator[](intptr_t i) const { return at(i); }
-
-  const uint8_t* data() const { return data_; }
-  intptr_t num_elements() const { return num_elements_; }
-  Dart_Handle dart_handle() const { return dart_handle_; }
-
-  void Release();
-
- private:
-  uint8_t* data_;
-  intptr_t num_elements_;
-  Dart_Handle dart_handle_;
-
-  Uint8List(const Uint8List& other) = delete;
-};
-
-template <>
-struct DartConverter<Uint8List> {
-  static void SetReturnValue(Dart_NativeArguments args, Uint8List val);
-  static Uint8List FromArguments(Dart_NativeArguments args,
-                                 int index,
-                                 Dart_Handle& exception);
-  static Dart_Handle ToDart(const uint8_t* buffer, unsigned int length);
-};
-
-}  // namespace tonic
+#include "tonic/typed_data/typed_list.h"
 
 #endif  // LIB_TONIC_TYPED_DATA_UINT8_LIST_H_
